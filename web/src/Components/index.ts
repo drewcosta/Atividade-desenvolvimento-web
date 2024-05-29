5741d93363e881c4a4302d6b2b2d067dc1756d1f
Andrew Costa <andrewcosta.contato@gmail.com>
2024-05-29 17:34:41 -0300

    feat: FormAuth/implement logic for authentication redirection from the path

---
+++ web/src/Components/index.ts
@@ -0,0 +1,5 @@
+import { Divider } from './Divider';
+import { FormAuth } from './FormAuth';
+import { DefaultLayout } from './Layout';
+
+export { DefaultLayout, Divider, FormAuth };
---
+++ web/src/Pages/index.ts
@@ -0,0 +1,6 @@
+import { Landing } from "./Landing";
+import { Register } from "./Register";
+
+
+
+export { Landing, Register };

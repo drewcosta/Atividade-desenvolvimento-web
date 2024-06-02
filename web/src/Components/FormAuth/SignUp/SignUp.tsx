6b634fb91906218567f6ae2bf042881c3b7b6182
Andrew Costa <andrewcosta.contato@gmail.com>
2024-06-01 21:45:32 -0300

    feat: signup/create form and validation for registration

--- web/src/Components/FormAuth/SignUp/SignUp.tsx
+++ web/src/Components/FormAuth/SignUp/SignUp.tsx
@@ -1,30 +1,118 @@
 import './SignUp.modules.css';
+import { ChangeEvent, FormEventHandler, useState } from 'react';
 
+import { useAuth } from '../../../Hooks';
+
+// components
 import { Divider } from '../..//Divider';
 
 // bootstrap components
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+interface FormState {
+  email: string;
+  password: string;
+}
+
 export function SignUp() {
+  const { signup } = useAuth();
+
+  const [form, setForm] = useState<FormState>({
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState<Record<string, string | null>>({});
+
+  const [hasUser, setHasUser] = useState();
+
+  const setField = (field: string, value: string) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
+
+    if (!!errors[field]) {
+      setErrors({
+        ...errors,
+        [field]: null,
+      })
+    }
+  }
+
+  const validateForm = () => {
+    const { email, password } = form;
+    const newErrors: Record<string, string> = {};
+
+    if (!email || email === '' || !validateField(email, /^[^\s@]+@[^\s@]+\.[^\s@]+$/)) newErrors.email = "Insira um email válido."
+    if (!password || password === '' || !validateField(password, /^.{4,20}$/)) newErrors.password = "A senha deve ter entre 4 e 20 caracteres."
+
+    return newErrors;
+  }
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    const formErrors = validateForm();
+
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+
+      const res = signup(form.email, form.password);
+
+      if (res) {
+        return console.log(res)
+      }
+      resetForm();
+    }
+  }
+
+  const validateField = (value: string, regex: RegExp): boolean => {
+    return regex.test(value);
+  };
+
+  const resetForm = () => {
+    const resetFields = Object.keys(form).reduce((allKeys, key) => {
+      allKeys[key as keyof FormState] = '';
+      return allKeys;
+    }, {} as FormState);
+
+    setForm(resetFields);
+  }
 
   return (
     <div className="container d-flex flex-column justify-content-center align-items-center __container">
       <div className='container__content'>
-        <Form className='content__form border'>
-          <h1 className='h3 text-center'>Cadastrar</h1>
-          <p className='text-center'>Preencha os dados abaixo para concluir o cadastro.</p>
+        <Form className='content__form border' onSubmit={handleSubmit}>
+          <h1 className='h3 text-center'>Acesso à Lista de compras</h1>
+          <p className='text-center'>Preencha os dados abaixo para o acesso.</p>
 
           <Divider />
 
-          <Form.Group className="mb-3 w-100" controlId="formBasicEmail">
+          <Form.Group className="mb-3 w-100" controlId="email">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Digite o email" />
+            <Form.Control
+              type="email"
+              placeholder="Digite o email"
+              value={form.email}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setField('email', e.target.value)}
+              isInvalid={!!errors.email}
+            />
+            <Form.Control.Feedback type='invalid'>{errors.email}</Form.Control.Feedback>
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="mb-3" controlId="password">
             <Form.Label>Senha</Form.Label>
-            <Form.Control type="password" placeholder="Digite a senha" />
+            <Form.Control
+              type="password"
+              placeholder="Digite a senha"
+              value={form.password}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setField('password', e.target.value)}
+              isInvalid={!!errors.password}
+            />
+            <Form.Control.Feedback type='invalid'>{errors.password}</Form.Control.Feedback>
           </Form.Group>
 
           <Button variant="success" className='w-100' type="submit">
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import router from './Routes/Router';
+import { UserAuthProvider } from './Contexts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserAuthProvider>
+      <RouterProvider router={router} />
+    </UserAuthProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
